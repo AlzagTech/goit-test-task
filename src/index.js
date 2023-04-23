775b1925705d73e0629c277b2624bcab0c3ad602
Oleksandr Zahorskyi <alex.alzag.zagorsky@gmail.com>
2023-04-23 22:06:58 +0300

    path update

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/tweets">
+        <BrowserRouter basename="/goit-test-task">
           <App />
         </BrowserRouter>
       </PersistGate>
